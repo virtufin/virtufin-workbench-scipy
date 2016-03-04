eed12fa103d964cdb86e9093785b0fb74d21821a
Patrick Haener <contact@haenerconsulting.com>
2016-03-04 11:03:12 +0100

    Add HOSTNAME_COMMAND

--- variables.sh
+++ variables.sh
@@ -1,4 +1,4 @@
 export BASEDIR=`pwd -P`
 export JPY_COOKIE_SECRET=`openssl rand -hex 1024`
 export CONFIGPROXY_AUTH_TOKEN=`openssl rand -hex 32`
-export KAFKA_ADVERTISED_HOST_NAME=`hostname`
+export HOSTNAME_COMMAND="resolveip -s `hostname`"
