45eabbb4878a75924be149057f66e08e51fba09e
Patrick Haener <contact@haenerconsulting.com>
2016-03-07 17:30:15 +0100

    Update kafka.yml; config for apache2

--- variables.sh
+++ variables.sh
@@ -1,4 +1,8 @@
 export BASEDIR=`pwd -P`
 export JPY_COOKIE_SECRET=`openssl rand -hex 1024`
 export CONFIGPROXY_AUTH_TOKEN=`openssl rand -hex 32`
-export HOSTNAME_COMMAND="resolveip -s `hostname`"
+export KAFKA_ADVERTISED_HOST_NAME=kafka
+#export HOSTNAME_COMMAND="resolveip -s `hostname`"
+export KAFKA_ADVERTISED_PORT=9092
+export ADVERTISED_PORT=9092
+export KAFKA_ZOOKEEPER_CONNECT=zk:2181
